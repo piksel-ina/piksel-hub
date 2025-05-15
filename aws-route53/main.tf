7de2dba79e86c44e0cefed8d59905253ffd6174b
taufik-shf <taufik-shf@gmail.com>
2025-05-15 21:37:53 +0700

    Add egress cidr to prevent drifts

--- aws-route53/main.tf
+++ aws-route53/main.tf
@@ -152,7 +152,8 @@ module "inbound_resolver_endpoint" {
   security_group_name                = "${var.project}-resolver-inbound-sg"
   security_group_description         = "Allow DNS queries to Inbound Resolver Endpoint for ${var.project}"
   security_group_ingress_cidr_blocks = concat([var.spoke_vpc_cidrs_dev], [var.vpc_cidr_block_shared])
-
+  security_group_egress_cidr_blocks  = ["0.0.0.0/0"]
+  
   tags                = merge(local.tags, { Name = "${var.project}-inbound-resolver" })
   security_group_tags = merge(local.tags, { Name = "${var.project}-resolver-inbound-sg" })
 }
