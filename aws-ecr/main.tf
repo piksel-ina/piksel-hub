e4949ce72ed09743ea811f9522db733d40badad1
taufik-shf <taufik.shf@gmail.com>
2025-05-27 11:34:48 +0700

    Make ECR lifecycle rule specific for each image and environment

--- aws-ecr/main.tf
+++ aws-ecr/main.tf
@@ -20,36 +20,67 @@ resource "aws_ecr_lifecycle_policy" "this" {
   repository = aws_ecr_repository.this.name
   policy = jsonencode({
     rules = [
+      // --- Production and Staging Images rule priority 1 to 20 ---
       {
-        "rulePriority" : 1,
-        "description" : "Keep last 15 production (vX.Y.Z) and recent staging (vX.Y.Z-betaN) images",
-        "selection" : {
-          "tagStatus" : "tagged",
-          "tagPrefixList" : ["v"],
-          "countType" : "imageCountMoreThan",
-          "countNumber" : 15
+        rulePriority = 1,
+        description  = "Keep last 15 ODC prod/staging images (vX.Y.Z, vX.Y.Z-betaN)",
+        selection = {
+          tagStatus     = "tagged",
+          tagPrefixList = ["odc-v"],
+          countType     = "imageCountMoreThan",
+          countNumber   = 15
         },
-        "action" : {
-          "type" : "expire"
+        action = {
+          type = "expire"
         }
       },
       {
-        rulePriority = 2
-        description  = "Expire dev branch images (e.g., feature-*-latest, develop-latest) not updated in 45 days"
+        rulePriority = 2,
+        description  = "Keep last 10 Jupyter prod/staging images (vX.Y.Z, vX.Y.Z-betaN)",
         selection = {
-          tagStatus     = "tagged"
-          tagPrefixList = ["feature-", "develop-", "main-", "dev-"],
-          countType     = "sinceImagePushed",
-          countUnit     = "days",
-          countNumber   = 45
-        }
+          tagStatus     = "tagged",
+          tagPrefixList = ["jupyter-v"],
+          countType     = "imageCountMoreThan",
+          countNumber   = 10
+        },
         action = {
           type = "expire"
         }
       },
 
+      // --- Development Images rule priority 21 to 40 ---
       {
-        rulePriority = 10
+        rulePriority = 21,
+        description  = "Expire ODC dev branch images not updated in 45 days",
+        selection = {
+          tagStatus     = "tagged",
+          tagPrefixList = ["odc-feature-", "odc-develop-", "odc-main-", "odc-dev-"],
+          countType     = "sinceImagePushed",
+          countUnit     = "days",
+          countNumber   = 45
+        },
+        action = {
+          type = "expire"
+        }
+      },
+      {
+        rulePriority = 22,
+        description  = "Expire Jupyter dev branch images not updated in 30 days",
+        selection = {
+          tagStatus     = "tagged",
+          tagPrefixList = ["jupyter-feature-", "jupyter-develop-", "jupyter-main-", "jupyter-dev-"],
+          countType     = "sinceImagePushed",
+          countUnit     = "days",
+          countNumber   = 30
+        },
+        action = {
+          type = "expire"
+        }
+      },
+
+      // --- Other rules ---
+      {
+        rulePriority = 90
         description  = "Expire untagged images older than 7 days"
         selection = {
           tagStatus   = "untagged"
@@ -57,6 +88,20 @@ resource "aws_ecr_lifecycle_policy" "this" {
           countUnit   = "days"
           countNumber = 7
         }
+        action = {
+          type = "expire"
+        }
+      },
+      {
+        rulePriority = 99, # Ensure this is the last rule
+        description  = "Expire any other tagged images not matching above rules after 60 days",
+        selection = {
+          tagStatus      = "tagged",
+          tagPatternList = ["*"],
+          countType      = "sinceImagePushed",
+          countUnit      = "days",
+          countNumber    = 60
+        },
         action = {
           type = "expire"
         }
