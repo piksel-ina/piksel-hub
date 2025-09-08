15be76250f75277315db0f59d17788e64196996a
taufik-shf <taufik.shf@gmail.com>
2025-09-08 14:00:00 +0700

    Clean old and irrelevant configuration

--- aws-route53-association/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "authorization_ids" {
-  description = "The calculated unique identifiers for the association"
-  value       = { for k, v in aws_route53_vpc_association_authorization.this : k => v.id }
-}
